20dcda4ba368a9e03669aaade94bc7723ca061a5
Harikrishnan <harikrishnankannoth@gmail.com>
2023-01-19 23:03:40 +0530

    CONFIGURED AND USER INPUT ADDED

--- client.c
+++ client.c
@@ -6,15 +6,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char **argv){
-
-  if (argc != 2) {
-    printf("Usage: %s <port>\n", argv[0]);
-    exit(0);
-  }
+int main(){
 
   char *ip = "127.0.0.1";
-  int port = atoi(argv[1]);
+  int port = 5566;
+
+  
 
   int sockfd;
   struct sockaddr_in addr;
@@ -34,12 +31,13 @@ int main(int argc, char **argv){
   bzero(buffer, 1024);
   strcpy(buffer, temp);
   sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, sizeof(addr));
-  printf("[+]Data send: %s\n", buffer);
+  printf("\n[+]Data send: %s\n", buffer);
 
   bzero(buffer, 1024);
   addr_size = sizeof(addr);
   recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, &addr_size);
-  printf("[+]Data recv: %s\n", buffer);
+  printf("[+]Data recieved: %s\n", buffer);
 
+  printf("\n[+]Client disconnected.\n\n");
   return 0;
 }
--- server.c
+++ server.c
@@ -6,15 +6,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char **argv){
-
-  if (argc != 2){
-    printf("Usage: %s <port>\n", argv[0]);
-    exit(0);
-  }
+int main(){
 
   char *ip = "127.0.0.1";
-  int port = atoi(argv[1]);
+  int port = 5566;
+
+  
 
   int sockfd;
   struct sockaddr_in server_addr, client_addr;
@@ -27,7 +24,7 @@ int main(int argc, char **argv){
     perror("[-]socket error");
     exit(1);
   }
-
+   printf("[+]UDP server socket created.\n");  
   memset(&server_addr, '\0', sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(port);
@@ -39,17 +36,30 @@ int main(int argc, char **argv){
     exit(1);
   }
 
+   printf("[+]Bind to the port number: %d\n", port);
+
+
   bzero(buffer, 1024);
   addr_size = sizeof(client_addr);
   recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
-  printf("[+]Data recv: %s\n", buffer);
+  printf("\n[+]Data recieved: %s\n", buffer);
 
-    
+    int len = strlen(buffer);
+    for(int i=0;i<len/2;i++)
+    {
+        char temp = buffer[i];
+        buffer[i]=buffer[len-1-i];
+        buffer[len-1-i]=temp;
 
-  bzero(buffer, 1024);
-  strcpy(buffer, "Welcome to the UDP Server.");
+    }
+
+
+  //bzero(buffer, 1024);
+  //strcpy(buffer, "Welcome to the UDP Server.");
   sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
   printf("[+]Data send: %s\n", buffer);
+
+  printf("\n[+]Server disconnected.\n\n");
 
   return 0;
 }
